5de694d7e1290425e47cadeac3fd49e2e067cb43
Faith <pfaithmtan@gmail.com>
2018-09-14 18:00:08 -0700

    Finish validations for users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, :password_digest, :session_token, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6, allow_nil: true }, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
   
   attr_reader :password
   #giving user a username and password
